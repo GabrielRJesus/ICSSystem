a8240d1aad3c81216b16c86df8f867ce5982d810
Gabriel Jesus <gabrielrj623@hotmail.com>
2018-05-25 13:47:49 -0300

    Att Tela 002

---
+++ IceCreamSystem/src/view/TelaLoginController.java
@@ -0,0 +1,34 @@
+package view;
+
+import com.jfoenix.controls.JFXButton;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+
+public class TelaLoginController implements Initializable {
+
+    @FXML
+    private JFXButton btEntrar;
+    @FXML
+    private JFXButton btEsqueci;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void clkEntrar(ActionEvent event) {
+    }
+
+    @FXML
+    private void clkEsqueci(ActionEvent event) {
+    }
+    
+}

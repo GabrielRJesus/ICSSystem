943a9f28210b28ef0ce4d1fa94169cef458e9e46
GabrielRJesus <gabrielrj623@hotmail.com>
2018-11-09 19:18:57 -0200

    Relatório

--- IceCreamSystem/src/view/TelaPrincipalController.java
+++ IceCreamSystem/src/view/TelaPrincipalController.java
@@ -12,13 +12,11 @@ import exception.ControlException;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -125,8 +123,6 @@ public class TelaPrincipalController implements Initializable {
                 locFuncionario.setVisible(false);
             }
         }
-        
-        
         
     } 
     
